f923cad6eaed593ce8e1298cf058d27d3590b82d
killyoudead48 <seansmathers@gmail.com>
2025-06-22 21:23:29 -0500

    Update index.js

--- predecessor-live/server/index.js
+++ predecessor-live/server/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.WebsocketServer({ server });
 
 let clients = [];
 
