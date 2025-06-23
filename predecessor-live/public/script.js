962d4db19b489fca5dfc5a377fb57a644aab38c5
killyoudead48 <seansmathers@gmail.com>
2025-06-22 22:11:51 -0500

    Delete predecessor-live directory

--- predecessor-live/public/script.js
+++
@@ -1,7 +0,0 @@
-const output = document.getElementById('output');
-const ws = new WebSocket(`ws://${location.host}`);
-
-ws.onmessage = event => {
-  const data = JSON.parse(event.data);
-  output.textContent += JSON.stringify(data, null, 2) + '\n\n';
-};
